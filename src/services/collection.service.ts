79ab2659a32a7fd5dac54426351fdd279fe18a5a
Stephan <stepmix123@gmail.com>
2023-05-23 09:01:17 -0400

    added collection service

---
+++ src/services/collection.service.ts
@@ -0,0 +1,27 @@
+import graphQLClient from "@/graphql";
+import { COLLECTION_BY_HANDLE_QUERY } from "@/graphql/queries/collection-by-handle.query";
+import { COLLECTIONS_QUERY } from "@/graphql/queries/collections.query";
+
+class CollectionService {
+  static async getCollections() {
+    const collections = await graphQLClient.request(COLLECTIONS_QUERY, {
+      first: 10,
+      after: null,
+    });
+
+    return collections;
+  }
+
+  static async getCollectionByHandle(handle: string) {
+    const collectionByHandle = await graphQLClient.request(
+      COLLECTION_BY_HANDLE_QUERY,
+      {
+        handle,
+      }
+    );
+
+    return collectionByHandle;
+  }
+}
+
+export default Object.freeze(CollectionService);
