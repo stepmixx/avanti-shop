1f706d2b6a7c4efc35a8900990d1c0f74b209ec5
Stephan <stepmix123@gmail.com>
2023-05-24 10:57:32 -0400

    quick fix footer

--- src/components/layout/footer.tsx
+++ src/components/layout/footer.tsx
@@ -2,7 +2,7 @@ import { Typography } from "@/components/material-tailwind";
 
 function Footer() {
   return (
-    <footer className="bg-black text-white w-full py-6 mt-8 flex justify-center items-center">
+    <footer className="bg-black text-white w-full p-6 flex justify-center items-center">
       <Typography variant="small" className="text-center">
         © 2023, AVANTI. This is a mock for a take home task.
       </Typography>
