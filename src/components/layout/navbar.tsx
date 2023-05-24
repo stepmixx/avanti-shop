e2b817c78d89b972549a98dfcaed06a09c923f00
Stephan <stepmix123@gmail.com>
2023-05-24 07:36:49 -0400

    added navbar

---
+++ src/components/layout/navbar.tsx
@@ -0,0 +1,70 @@
+"use client";
+import React from "react";
+import Sidenav from "./sidenav";
+import Image from "next/image";
+import { Button, IconButton } from "../material-tailwind";
+import { IconMenu2, IconSearch, IconUserCircle } from "@tabler/icons-react";
+import Link from "next/link";
+
+function Navbar() {
+  const [sidenavOpen, setSidenavOpen] = React.useState(false);
+
+  const toggleSidenav = () => setSidenavOpen((prev) => !prev);
+
+  const closeSidenav = () => setSidenavOpen(false);
+
+  const ROUTES = [
+    {
+      title: "Home",
+      path: "/",
+    },
+    {
+      title: "Collections",
+      path: "/collections",
+    },
+    {
+      title: "Products",
+      path: "/products",
+    },
+  ];
+
+  return (
+    <>
+      <nav className="fixed top-0 left-0 grid grid-cols-3 items-center px-4 sm:px-6 lg:px-8 bg-black text-white w-full h-16 shadow-md">
+        <IconButton
+          variant="text"
+          color="gray"
+          className="lg:hidden"
+          onClick={toggleSidenav}
+        >
+          <IconMenu2 className="h-6 w-6 text-white" />
+        </IconButton>
+        <div className="hidden lg:block">
+          <ul className="flex gap-1">
+            {ROUTES.map(({ title }) => (
+              <li key={title}>
+                <Button variant="text" color="white" className="p-2">
+                  <p className="capitalize text-lg font-thin">{title}</p>
+                </Button>
+              </li>
+            ))}
+          </ul>
+        </div>
+        <Link href="/" className="mx-auto">
+          <Image src="/AVANTI.svg" alt="Avanti logo" height={48} width={96} />
+        </Link>
+        <div className="flex gap-2 sm:gap-4 items-center ml-auto">
+          <IconButton variant="text" color="gray" onClick={toggleSidenav}>
+            <IconSearch className="h-6 w-6 text-white" />
+          </IconButton>
+          <IconButton variant="text" color="gray" onClick={toggleSidenav}>
+            <IconUserCircle className="h-6 w-6 text-white" />
+          </IconButton>
+        </div>
+      </nav>
+      <Sidenav routes={ROUTES} open={sidenavOpen} onClose={closeSidenav} />
+    </>
+  );
+}
+
+export default Navbar;
---
+++ src/components/layout/sidenav.tsx
@@ -0,0 +1,62 @@
+import React from "react";
+import Link from "next/link";
+import Image from "next/image";
+import { usePathname } from "next/navigation";
+import { Button, Drawer, IconButton } from "@/components/material-tailwind";
+import { IconX } from "@tabler/icons-react";
+
+interface SidenavProps {
+  open: boolean;
+  onClose: () => void;
+  routes: { title: string; path: string }[];
+}
+
+export function Sidenav(props: SidenavProps) {
+  const { open, onClose, routes } = props;
+  const pathname = usePathname();
+
+  return (
+    <>
+      <Drawer open={open} onClose={onClose} className="bg-black p-2">
+        <div className={`relative border-b border-white/20`}>
+          <Link href="/" className="flex items-center py-4">
+            <Image src="/AVANTI.svg" alt="Avanti logo" height={48} width={96} />
+          </Link>
+          <IconButton
+            variant="text"
+            color="white"
+            size="sm"
+            ripple={false}
+            className="absolute right-0 top-0"
+            onClick={onClose}
+          >
+            <IconX className="h-5 w-5 text-white" />
+          </IconButton>
+        </div>
+        <div className="m-4">
+          <ul className="mb-4 flex flex-col gap-2">
+            {routes.map(({ title, path }) => {
+              const isActive = pathname === path;
+              return (
+                <li key={title}>
+                  <Link href={path}>
+                    <Button
+                      color="white"
+                      variant={isActive ? "gradient" : "text"}
+                      className="flex items-center px-4"
+                      fullWidth
+                    >
+                      <p className="normal-case text-lg font-normal">{title}</p>
+                    </Button>
+                  </Link>
+                </li>
+              );
+            })}
+          </ul>
+        </div>
+      </Drawer>
+    </>
+  );
+}
+
+export default Sidenav;
