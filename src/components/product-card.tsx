94d017cf62680f1bced4e5de4b1c3b025d3a9288
Stephan <stepmix123@gmail.com>
2023-05-24 07:37:13 -0400

    added product card

---
+++ src/components/product-card.tsx
@@ -0,0 +1,38 @@
+import Link from "next/link";
+import { Typography } from "./material-tailwind";
+import Image from "next/image";
+
+interface ProductCardProps {
+  product: any;
+  size?: number;
+}
+
+function ProductCard(props: ProductCardProps) {
+  const { product, size = 256 } = props;
+
+  return (
+    <Link href={`/product/${product.handle}`}>
+      <div
+        style={{ width: size, height: size + 64 }}
+        className={`grid grid-rows-[1fr_64px] shadow rounded-sm`}
+      >
+        <div className="relative w-full h-full">
+          <Image
+            src={product.featuredImage.url}
+            alt={product.featuredImage.altText}
+            fill
+          />
+        </div>
+        <div className="w-full h-16 px-3 flex items-center justify-between">
+          <Typography variant="h6">{product.title}</Typography>
+          <Typography
+            variant="h6"
+            className="font-bold"
+          >{`${product.price.amount} ${product.price.currencyCode}`}</Typography>
+        </div>
+      </div>
+    </Link>
+  );
+}
+
+export default ProductCard;
