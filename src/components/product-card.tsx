d7e65e007b75686d15458c4b1d66fdd705ff06cb
Stephan <stepmix123@gmail.com>
2023-05-24 15:02:04 -0400

    added cart button to product card

--- src/components/product-card.tsx
+++ src/components/product-card.tsx
@@ -1,6 +1,8 @@
-import Link from "next/link";
-import { Typography } from "./material-tailwind";
+"use client";
+import { IconButton, Typography } from "./material-tailwind";
 import Image from "next/image";
+import { IconShoppingCart } from "@tabler/icons-react";
+import { useRouter } from "next/navigation";
 
 interface ProductCardProps {
   product: any;
@@ -9,25 +11,33 @@ interface ProductCardProps {
 
 function ProductCard(props: ProductCardProps) {
   const { product, size = 256 } = props;
+  const router = useRouter();
 
   return (
-    <Link href={`/products/${product.handle}`}>
-      <div
-        style={{ width: size, height: size + 64 }}
-        className={`grid grid-rows-[1fr_64px] shadow rounded-sm`}
-      >
-        <div className="relative w-full h-full">
-          <Image src={product.featuredImage.url} alt={product.title} fill />
-        </div>
-        <div className="w-full h-16 px-3 flex items-center justify-between">
-          <Typography variant="h6">{product.title}</Typography>
-          <Typography
-            variant="h6"
-            className="font-bold text-right"
-          >{`${product.price.amount} ${product.price.currencyCode}`}</Typography>
+    <div
+      onClick={() => router.push(`/products/${product.handle}`)}
+      style={{ width: size, height: size + 64 }}
+      className={`grid grid-rows-[1fr_64px] shadow rounded-sm hover:cursor-pointer`}
+    >
+      <div className="relative w-full h-full">
+        <Image src={product.featuredImage.url} alt={product.title} fill />
+      </div>
+      <div className="w-full h-16">
+        <Typography className="text-center font-bold" variant="h6">
+          {product.title}
+        </Typography>
+        <div className="flex px-4 items-center justify-between">
+          <Typography variant="h6">{`${product.price.amount} ${product.price.currencyCode}`}</Typography>
+          <IconButton
+            variant="filled"
+            onClick={() => alert("Item added to cart")}
+            className="w-8 h-8 bg-black rounded-full"
+          >
+            <IconShoppingCart className="w-5 text-white" />
+          </IconButton>
         </div>
       </div>
-    </Link>
+    </div>
   );
 }
 
