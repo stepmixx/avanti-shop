1f43b96779a1acc7412e869e5ec754d0df989a77
Stephan <stepmix123@gmail.com>
2023-05-24 10:57:58 -0400

    added cart icon to navbar

--- src/components/layout/navbar.tsx
+++ src/components/layout/navbar.tsx
@@ -3,7 +3,12 @@ import React from "react";
 import Sidenav from "./sidenav";
 import Image from "next/image";
 import { Button, IconButton } from "../material-tailwind";
-import { IconMenu2, IconSearch, IconUserCircle } from "@tabler/icons-react";
+import {
+  IconMenu2,
+  IconSearch,
+  IconShoppingCart,
+  IconUserCircle,
+} from "@tabler/icons-react";
 import Link from "next/link";
 
 function Navbar() {
@@ -14,10 +19,6 @@ function Navbar() {
   const closeSidenav = () => setSidenavOpen(false);
 
   const ROUTES = [
-    {
-      title: "Home",
-      path: "/",
-    },
     {
       title: "Collections",
       path: "/collections",
@@ -30,14 +31,14 @@ function Navbar() {
 
   return (
     <>
-      <nav className="fixed top-0 left-0 z-[100] grid grid-cols-3 items-center px-4 sm:px-6 lg:px-8 bg-black text-white w-full h-16 shadow-md">
+      <nav className="fixed top-0 left-0 z-[100] grid grid-cols-3 items-center px-2 sm:px-6 lg:px-8 bg-black text-white w-full h-16 shadow-md">
         <IconButton
           variant="text"
           color="gray"
-          className="lg:hidden"
+          className="w-8 sm:w-10 h-8 sm:h-10 lg:hidden"
           onClick={toggleSidenav}
         >
-          <IconMenu2 className="h-6 w-6 text-white" />
+          <IconMenu2 className="w-5 sm:w-6 text-white" />
         </IconButton>
         <div className="hidden lg:block">
           <ul className="flex gap-1">
@@ -53,12 +54,30 @@ function Navbar() {
         <Link href="/" className="mx-auto">
           <Image src="/AVANTI.svg" alt="Avanti logo" height={48} width={96} />
         </Link>
-        <div className="flex gap-2 sm:gap-4 items-center ml-auto">
-          <IconButton variant="text" color="gray" onClick={toggleSidenav}>
-            <IconSearch className="h-6 w-6 text-white" />
+        <div className="flex gap-1 sm:gap-4 items-center ml-auto">
+          <IconButton
+            color="gray"
+            variant="text"
+            onClick={() => alert("Buscar")}
+            className="w-8 sm:w-10 h-8 sm:h-10"
+          >
+            <IconSearch className="w-5 sm:w-6 text-white" />
           </IconButton>
-          <IconButton variant="text" color="gray" onClick={toggleSidenav}>
-            <IconUserCircle className="h-6 w-6 text-white" />
+          <IconButton
+            color="gray"
+            variant="text"
+            onClick={() => alert("Cart")}
+            className="w-8 sm:w-10 h-8 sm:h-10"
+          >
+            <IconShoppingCart className="w-5 sm:w-6 text-white" />
+          </IconButton>
+          <IconButton
+            color="gray"
+            variant="text"
+            onClick={() => alert("User")}
+            className="w-8 sm:w-10 h-8 sm:h-10"
+          >
+            <IconUserCircle className="w-5 sm:w-6 text-white" />
           </IconButton>
         </div>
       </nav>
