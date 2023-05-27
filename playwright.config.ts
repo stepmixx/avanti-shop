e1cbe01a11060a645ce584e87a59b7a3bb82755a
Stephan <stepmix123@gmail.com>
2023-05-27 02:07:50 -0400

    playwright config changes

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,41 +1,40 @@
-import { defineConfig, devices } from "@playwright/test";
+import { defineConfig } from "@playwright/test";
 
 const baseURL = `http://localhost:${process.env.PORT || 3000}`;
 export default defineConfig({
   testDir: "e2e",
   retries: 2,
   webServer: {
-    command: "pnpm build && pnpm start",
+    command: "pnpm dev", // has to be pnpm build && pnpm start for production.
     url: baseURL,
     timeout: 120 * 1000,
     reuseExistingServer: !process.env.CI,
   },
   use: {
     baseURL,
-    trace: "retry-with-trace",
   },
-  projects: [
-    {
-      name: "Desktop Chrome",
-      use: {
-        ...devices["Desktop Chrome"],
-      },
-    },
-    {
-      name: "Desktop Safari",
-      use: {
-        ...devices["Desktop Safari"],
-      },
-    },
-    {
-      name: "Mobile Chrome",
-      use: {
-        ...devices["Pixel 5"],
-      },
-    },
-    {
-      name: "Mobile Safari",
-      use: devices["iPhone 12"],
-    },
-  ],
+  // projects: [
+  //   {
+  //     name: "Desktop Chrome",
+  //     use: {
+  //       ...devices["Desktop Chrome"],
+  //     },
+  //   },
+  //   {
+  //     name: "Desktop Safari",
+  //     use: {
+  //       ...devices["Desktop Safari"],
+  //     },
+  //   },
+  //   {
+  //     name: "Mobile Chrome",
+  //     use: {
+  //       ...devices["Pixel 5"],
+  //     },
+  //   },
+  //   {
+  //     name: "Mobile Safari",
+  //     use: devices["iPhone 12"],
+  //   },
+  // ],
 });
