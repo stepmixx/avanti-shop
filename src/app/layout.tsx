05c009e2e2d56a9f8ab02edb6691e33bc0f7928d
Stephan <stepmix123@gmail.com>
2023-06-06 23:08:35 -0400

    added metadata to layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -9,6 +9,50 @@ const montserrat = Montserrat({
   display: "swap",
 });
 
+const APP_NAME = "Take Home | AVANTI";
+const APP_DESCRIPTION = "Take home assignment for Avanti";
+
+export function metadata() {
+  return {
+    applicationName: APP_NAME,
+    description: APP_DESCRIPTION,
+    keywords: [
+      "AVANTI",
+      "Take Home",
+      "Gallery",
+      "e-commerce",
+      "shop",
+      "store",
+      "galeria AVANTI",
+    ],
+    authors: [{ name: "Stephan" }],
+    colorScheme: "dark",
+    creator: "Stephan",
+    manifest: "/manifest.json",
+    themeColor: "#FFFFFF",
+    metadataBase: new URL("https://avanti-take-home.vercel.app"),
+    appleWebApp: {
+      capable: true,
+      statusBarStyle: "default",
+      title: APP_NAME,
+    },
+    formatDetection: {
+      telephone: false,
+    },
+    openGraph: {
+      type: "website",
+      siteName: APP_NAME,
+      title: APP_NAME,
+      description: APP_DESCRIPTION,
+    },
+    twitter: {
+      card: "summary",
+      title: APP_NAME,
+      description: APP_DESCRIPTION,
+    },
+  };
+}
+
 export default function RootLayout({
   children,
 }: {
